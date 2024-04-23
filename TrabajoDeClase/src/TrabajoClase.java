175ed40cece4e375687180fc07f4b2ac77439b7c
carlos1086 <167225814+carlos1086@users.noreply.github.com>
2024-04-23 18:44:02 +0200

    Update TrabajoClase.java
    new

--- TrabajoDeClase/src/TrabajoClase.java
+++ TrabajoDeClase/src/TrabajoClase.java
@@ -4,7 +4,7 @@ public class TrabajoClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola soy elizabeth");
-
+		System.out.println("Hola Me llamo Carlos");
 	}
 
 }
